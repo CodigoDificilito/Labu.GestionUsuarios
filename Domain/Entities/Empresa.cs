b4a597bf7d1d2d761f0d66d9cc6f34edeb4cd363
ivanbrestt <ivanbrestt@gmail.com>
2023-04-20 16:35:11 -0300

    Scrips EF Core generados

--- Domain/Entities/Empresa.cs
+++ Domain/Entities/Empresa.cs
@@ -8,6 +8,20 @@ namespace Domain.Entities
 {
     public class Empresa
     {
+        public int EmpresaId { get; set; }
+        public int UsuarioId { get; set; }
+        public string Logo { get; set; }
+        public string Nombre { get; set; }
+        public string Provincia { get; set; }
+        public string Ciudad { get; set; }
+        public string Direccion { get; set; }
+        public string SectorEmpresarial { get; set; }
+        public string PaginaWeb { get; set; }
+        public string Descripcion { get; set; }
 
+
+        public Usuario Usuario { get; set; }
+        public DatosContacto DatosContacto { get; set; }
+        public IList<Recruiter> Recruiter { get; set; }
     }
 }
--- Domain/Entities/Recruiter.cs
+++ Domain/Entities/Recruiter.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Aspirante
+    public class Recruiter
     {
-        public int AspiranteId { get; set; }
+        public int RecruiterId { get; set; }
         public int PersonaId { get; set; }
+        public int EmpresaId { get; set; }
 
 
         public Persona Persona { get; set; }
+        public Empresa Empresa { get; set; }
     }
 }
